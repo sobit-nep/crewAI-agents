db2d18a5657db1c3bd15ac152b6babbfdcdbb9eb
Shovit Nepal <65544518+sobit-nep@users.noreply.github.com>
2024-09-26 12:09:12 +0545

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from match_to_proposal.crew import MatchToProposalCrew
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'path_to_jobs_csv': './src/match_to_proposal/data/jobs.csv',
-        'path_to_cv': './src/match_to_proposal/data/cv.md'
+        'path_to_jobs_csv': './data/jobs.csv',
+        'path_to_cv': '.data/cv.md'
     }
     MatchToProposalCrew().crew().kickoff(inputs=inputs)
 
